f13d520d3166d7176ac779abe27ea577c33b2c47
Ximones <chokcf-wm23@student.tarc.edu.my>
2024-08-27 23:50:32 +0800

    Adjusting function call from main
    1.Menu function now returning value to Main.
    2.Main call specific function based on returned value
    3.Screen Clearing function added

--- 8086/main.asm
+++ 8086/main.asm
@@ -1,21 +1,104 @@
 .MODEL SMALL
 .STACK 100
 .DATA 
+    choiceMenu DB ?
+    newLine DB 0DH,0AH,"$"    ; Newline definition
+	MSG1 db "STOCK IN$"
+	MSG2 db "STOCK OUT$"
+	MSG3 db "SEARCH$"
+	MSG4 db "GENERATE$"
+	MSG5 db "EXIT$"
 
 .CODE
 
-;-- INCLUDE the file path here to call function from different file  
+;-- INCLUDE the menu file
 INCLUDE Fs\menu.inc
+INCLUDE Simon\clnScr.inc
 
 MAIN PROC 
-	MOV AX,@DATA
-	MOV DS,AX
+    MOV AX,@DATA
+    MOV DS,AX
 
-	CALL user_Menu ;--- CALLING USER MENU FUNCTION
+menuLoop:
+    CALL user_Menu    ; Call the user menu function
 	
-	MOV AX,4C00H
-	INT 21H
+	CALL clear_Screen
+	
+    ; Go To Choice
+    CMP choiceMenu, 1
+    JE stockIn
+    CMP choiceMenu, 2
+    JE stockOut
+    CMP choiceMenu, 3
+    JE searchStock
+    CMP choiceMenu, 4
+    JE generateReport
+    CMP choiceMenu, 5
+    JE exitProgram
+
+	
+stockIn:
+    ; Code for Stock In
+	MOV AH,09H
+    LEA DX, MSG1
+    INT 21H
+	
+	MOV AH, 09H
+    LEA DX, newLine
+    INT 21H
+	
+	JMP menuLoop
+
+stockOut:
+    ; Code for Stock Out
+	MOV AH,09H
+    LEA DX, MSG2
+    INT 21H
+	
+	MOV AH, 09H
+    LEA DX, newLine
+    INT 21H
+	
+    JMP menuLoop
+
+searchStock:
+    ; Code for Search Stock
+	
+	MOV AH,09H
+    LEA DX, MSG3
+    INT 21H
+	
+	MOV AH, 09H
+    LEA DX, newLine
+    INT 21H
+	
+    JMP menuLoop
+
+generateReport:
+    ; Code for Generate Report
+	
+	MOV AH,09H
+    LEA DX, MSG4
+    INT 21H
+	
+	MOV AH, 09H
+    LEA DX, newLine
+    INT 21H
+	
+    JMP menuLoop
+
+exitProgram:
+
+	MOV AH,09H
+    LEA DX, MSG5
+    INT 21H
+	
+	MOV AH, 09H
+    LEA DX, newLine
+    INT 21H
+    
+	MOV AX, 4C00H
+    INT 21H
 
 MAIN ENDP
 END MAIN
-	
