ce4798473b70d7e02fed2a468814080365bdfc74
Zhen Bang <zhenbangqr@users.noreply.github.com>
2024-09-02 10:43:35 +0800

    Added stokin
    Write is not fully functional, it does not append correctly, rewrites all the time

--- 8086/stokin.asm
+++ 8086/stokin.asm
@@ -1,47 +1,104 @@
 .model small
-.stack 200h ; Increased stack size
+.stack 200h 
 
 .data
-    filename db "phone_inventory.txt", 0 
-    file_data db "POCO F5;16GB;256GB;BLUE;3000.00;20", 10
-               db "VIVO Y2;16GB;128GB;WHITE;3000.00;19", 10
-               db "VIVO Y3;16GB;128GB;WHITE;3000.00;18", 10
-               db "VIVO Y4;16GB;128GB;WHITE;3000.00;17", 10
-               db "VIVO Y5;16GB;128GB;WHITE;3000.00;16", 10
-               db "VIVO Y6;16GB;128GB;WHITE;3000.00;15", 10
-               db "VIVO Y7;16GB;128GB;WHITE;3000.00;14", 10
-               db "VIVO Y8;16GB;128GB;WHITE;3000.00;13", 10
-               db "VIVO Y9;16GB;128GB;WHITE;3000.00;12", 10
-               db "VIVO Y10;16GB;128GB;WHITE;3000.00;11", 10, '$' 
-    file_data_end: 
+    filename db "PHONE_IN.txt", 0 
+    file_data db 255 dup(0) 
     file_handle dw ?
+    newline db 13, 10 
+    prompt_model db "Enter phone model: $"
+    prompt_ram db "Enter RAM (in GB): $"
+    prompt_storage db "Enter storage (in GB): $"
+    prompt_color db "Enter color: $"
+    prompt_price db "Enter price: $"
+    prompt_quantity db "Enter quantity: $"
+    prompt_continue db "Do you want to add another phone (y/n)? $"
+    separator db ";", 0
 
 .code
 start:
     mov ax, @data
     mov ds, ax
 
-    ; Create or truncate the file for writing
-    mov ah, 3ch     
-    mov cx, 0      
+    ; Open the file in append mode 
+    mov ah, 3dh 
+    mov al, 2 ; Open file in read/write mode
+    or  al, 1000b ; Set the append flag
     lea dx, filename 
-    int 21h       
-
-    jc error      
-
-    mov [file_handle], ax 
-
-    ; Write the data to the file
-    mov ah, 40h    
-    mov bx, [file_handle]
-    mov cx, file_data_end - file_data 
-    lea dx, file_data 
     int 21h
 
-    jc error
+    ; Assuming the file exists, no need to check for carry flag
+    mov [file_handle], ax 
+
+    ; Set the file pointer to the end of the file for appending
+    mov ah, 42h        
+    mov al, 2          
+    xor cx, cx         
+    xor dx, dx         
+    mov bx, [file_handle]
+    int 21h
+
+input_loop:
+    ; Get phone model
+    lea dx, prompt_model
+    mov ah, 09h
+    int 21h
+    call get_input
+    call write_to_file
+
+    ; Get RAM
+    lea dx, prompt_ram
+    mov ah, 09h
+    int 21h
+    call get_input
+    call write_to_file
+
+    ; Get storage
+    lea dx, prompt_storage
+    mov ah, 09h
+    int 21h
+    call get_input
+    call write_to_file
+
+    ; Get color
+    lea dx, prompt_color
+    mov ah, 09h
+    int 21h
+    call get_input
+    call write_to_file
+
+    ; Get price
+    lea dx, prompt_price
+    mov ah, 09h
+    int 21h
+    call get_input
+    call write_to_file
+
+    ; Get quantity
+    lea dx, prompt_quantity
+    mov ah, 09h
+    int 21h
+    call get_input
+    call write_to_file
+
+    ; Add newline 
+    lea dx, newline
+    mov ah, 40h
+    mov bx, [file_handle]
+    mov cx, 2 ; Length of CR+LF
+    int 21h
+
+    ; Ask if user wants to add another phone
+    mov ah, 09h
+    lea dx, prompt_continue
+    int 21h
+    mov ah, 01h ; Get single character input
+    int 21h
+    cmp al, 'y'
+    je input_loop
 
     ; Close the file
-    mov ah, 3eh    
+    mov ah, 3eh 
     mov bx, [file_handle]
     int 21h
 
@@ -49,11 +106,31 @@ start:
     mov ah, 4ch
     int 21h
 
-error:
-    ; Handle errors (add your error handling code here)
-    ; For now, just terminate with a non-zero exit code
-    mov ah, 4ch
-    mov al, 1
+get_input proc
+    mov byte ptr file_data, 254 ; Max length of input (254 characters)
+    lea dx, file_data
+    mov ah, 0ah
     int 21h
-    
+    ret
+get_input endp
+
+write_to_file proc
+    ; Write user input to file
+    mov ah, 40h
+    mov bx, [file_handle]
+    lea dx, file_data + 2 ; Skip the first two bytes of input buffer
+    mov cx, 0
+    mov cl, [file_data + 1] ; Length of input
+    int 21h
+
+    lea dx, separator
+    mov ah, 40h
+    mov bx, [file_handle]
+    mov cx, 1
+    int 21h
+
+write_end:
+    ret
+write_to_file endp
+
 end start
